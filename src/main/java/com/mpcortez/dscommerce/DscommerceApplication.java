6a82229411d6237be025b192fd10610bd6117af1
Mpcortez <murilo_pass@hotmail.com>
2023-11-10 10:00:45 -0300

    Set app locale

--- src/main/java/com/mpcortez/dscommerce/DscommerceApplication.java
+++ src/main/java/com/mpcortez/dscommerce/DscommerceApplication.java
@@ -1,7 +1,11 @@
 package com.mpcortez.dscommerce;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Locale;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class DscommerceApplication {
@@ -10,4 +14,9 @@ public class DscommerceApplication {
         SpringApplication.run(DscommerceApplication.class, args);
     }
 
+    @PostConstruct
+    public void init() {
+        Locale.setDefault(new Locale("pt", "BR"));
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+    }
 }
