9338c1c9df2b71e21dc112dcd374bd8e0f286d75
Mpcortez <murilo_pass@hotmail.com>
2023-11-10 09:50:40 -0300

    Bean validations

--- src/main/java/com/mpcortez/dscommerce/dto/request/ProductRequestDTO.java
+++ src/main/java/com/mpcortez/dscommerce/dto/request/ProductRequestDTO.java
@@ -1,8 +1,16 @@
 package com.mpcortez.dscommerce.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
 public record ProductRequestDTO(Long id,
+                                @Size(min = 3, max = 80)
+                                @NotBlank
                                 String name,
+                                @Size(min = 10, message = "O campo deve ter no mínimo 10 caracteres.")
                                 String description,
+                                @Positive
                                 Double price,
                                 String imgUrl) {
 }
