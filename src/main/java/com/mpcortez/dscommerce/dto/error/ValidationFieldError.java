2af4cfdc162c89620d43b35ee7257116a1c23173
Mpcortez <murilo_pass@hotmail.com>
2023-11-10 09:45:01 -0300

    Errors handling

---
+++ src/main/java/com/mpcortez/dscommerce/dto/error/ValidationFieldError.java
@@ -0,0 +1,4 @@
+package com.mpcortez.dscommerce.dto.error;
+
+public record ValidationFieldError(String fieldName, String message) {
+}
---
+++ src/main/java/com/mpcortez/dscommerce/services/exception/DatabaseException.java
@@ -0,0 +1,8 @@
+package com.mpcortez.dscommerce.services.exception;
+
+public class DatabaseException extends RuntimeException {
+
+    public DatabaseException(String message) {
+       super(message);
+    }
+}
