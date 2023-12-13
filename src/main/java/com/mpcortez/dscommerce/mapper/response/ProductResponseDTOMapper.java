260d4cf24cdea197fccb44f699637bddbed89119
Mpcortez <murilo_pass@hotmail.com>
2023-12-13 14:08:11 -0300

    Product response without description

--- src/main/java/com/mpcortez/dscommerce/mapper/response/ProductResponseDTOMapper.java
+++ src/main/java/com/mpcortez/dscommerce/mapper/response/ProductResponseDTOMapper.java
@@ -5,13 +5,17 @@ import com.mpcortez.dscommerce.entities.Product;
 
 public class ProductResponseDTOMapper {
 
-    public static ProductResponseDTO mapper(Product product) {
+    public static ProductResponseDTO mapper(Product product, boolean description) {
         return new ProductResponseDTO(
                 product.getId(),
                 product.getName(),
-                product.getDescription(),
+                description ? product.getDescription() : "",
                 product.getPrice(),
                 product.getImgUrl()
         );
+    }
+
+    public static ProductResponseDTO mapper(Product product) {
+        return mapper(product, false);
     }
 }
